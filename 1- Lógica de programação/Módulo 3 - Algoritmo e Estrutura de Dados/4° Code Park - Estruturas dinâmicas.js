cffdc2969c5e821f180caacf96e5a0ab6b42348d
Alyson Alves <73599284+Alysonalves1@users.noreply.github.com>
2023-09-14 21:15:12 -0300

    Update 4° Code Park - Estruturas dinâmicas.js

--- 1- Lógica de programação/Módulo 3 - Algoritmo e Estrutura de Dados/4° Code Park - Estruturas dinâmicas.js
+++ 1- Lógica de programação/Módulo 3 - Algoritmo e Estrutura de Dados/4° Code Park - Estruturas dinâmicas.js
@@ -21,17 +21,17 @@ class Pessoa {
     }
 }
 
-// Criar instâncias de pessoas
+
 const pessoa1 = new Pessoa("João", 30);
 const pessoa2 = new Pessoa("Maria", 25);
 const pessoa3 = new Pessoa("Pedro", 5);
 const pessoa4 = new Pessoa("Laura", 3);
 
-// Construir a lista encadeada
-pessoa1.adicionarFilho(pessoa3); // João é pai de Pedro
-pessoa1.adicionarFilho(pessoa4); // João também é pai de Laura
 
-// Exemplo de como acessar informações na lista encadeada
+pessoa1.adicionarFilho(pessoa3); 
+pessoa1.adicionarFilho(pessoa4); 
+
+
 console.log(pessoa1.getInfo()); // Saída: Nome: João, Idade: 30, Filho(s): Pedro, Laura
 console.log(pessoa2.getInfo()); // Saída: Nome: Maria, Idade: 25
 console.log(pessoa3.getInfo()); // Saída: Nome: Pedro, Idade: 5
